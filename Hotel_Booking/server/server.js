5dfc49ba1b91a50e654833a1446d2dca0f353b1e
Vibha-1729 <vibhanarayan6174@gmail.com>
2026-03-04 01:18:20 +0530

    Updating webhooks

--- Hotel_Booking/server/server.js
+++ Hotel_Booking/server/server.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(clerkMiddleware())
 
 // API to listen to Clerk Webhooks
-app.use("/api/clerk",clerkWebhooks);
+app.post("/api/clerk",clerkWebhooks);
 
 app.get('/',(req,res)=> res.send("API is working fine"))
 
